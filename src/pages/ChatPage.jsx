affb1a8424340a70e71ff77d470cf5b0734fc45c
Adarsh Wilson <adarshwilsona@gmail.com>
2024-01-30 20:06:57 +0530

    beginnings of the chat page design

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import ChatPageStyles from '../styles/chat-page-styles.module.css'
 
+import logo from '../images/logo.png'
+
 const ChatPage = () => {
   return (
-    <div>ChatPage</div>
+    <div className={ChatPageStyles.chat_container}>
+        <SideBar />
+
+        <div className={ChatPageStyles.contacts_bar}></div>
+        <div className={ChatPageStyles.chat_bar}></div>
+    </div>
   )
+}
+
+/* SideBar Components:
+    -> Logo
+    -> Messages
+    -> Notifications
+    -> Contacts
+    -> Settings
+    -> Logout
+    -> Help
+*/
+function SideBar(){
+    return(
+    <div className={ChatPageStyles.side_bar}>
+        <img src={logo} alt="" className="logo" />
+        <div className={ChatPageStyles.messages_toggle}>
+            <button className={ChatPageStyles.side_button}></button>
+            <span className={ChatPageStyles.side_text}>Messages</span>
+        </div>
+        <div className={ChatPageStyles.notifs_toggle}>
+            <button className={ChatPageStyles.side_button}></button>
+            <span className={ChatPageStyles.side_text}>Notifs</span>
+        </div>
+        <div className={ChatPageStyles.contacts_toggle}>
+            <button className={ChatPageStyles.side_button}></button>
+            <span className={ChatPageStyles.side_text}>Contacts</span>
+        </div>
+        
+        <div className={ChatPageStyles.settings_toggle}>
+            <button className={ChatPageStyles.side_button}></button>
+            <span className={ChatPageStyles.side_text}>Settings</span>
+        </div>
+        <div className={ChatPageStyles.help_toggle}>
+            <button className={ChatPageStyles.side_button}></button>
+            <span className={ChatPageStyles.side_text}>Help</span>
+        </div>
+    </div>
+    )
 }
 
 export default ChatPage
