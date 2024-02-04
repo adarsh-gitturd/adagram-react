83df90e180cea1d2309408cb3580e48d23f7c11b
Adarsh Wilson <adarshwilsona@gmail.com>
2024-02-04 12:35:32 +0530

    more socket fiddling, but getting the hang of it now

--- spring-backend/src/main/java/com/wilson/adarsh/adagram/backend/serious_hello/WebSocketConfig.java
+++ spring-backend/src/main/java/com/wilson/adarsh/adagram/backend/serious_hello/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        // Sever -> Client
+        // Server -> Client
         config.setApplicationDestinationPrefixes("/app");
         // Client -> Server
     }
