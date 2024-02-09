466427d7d75267e8ccca0a5bc37a8de90583629f
Adarsh Wilson <adarshwilsona@gmail.com>
2024-02-09 18:13:44 +0530

    progress

--- spring-backend/src/main/java/com/wilson/adarsh/adagram/backend/Application.java
+++ spring-backend/src/main/java/com/wilson/adarsh/adagram/backend/Application.java
@@ -17,7 +17,7 @@ public class Application {
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
 				registry.addMapping("/abc")
 						.allowedOrigins("http://localhost:3000")
 						.allowedMethods("*")
